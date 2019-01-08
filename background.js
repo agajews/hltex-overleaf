8690f1f0cc58c6da9b5b2b8338b9b5e29f7a5237
Alex Gajewski <agajews@gmail.com>
2019-01-07 16:32:47 -0800

    fixed ev

--- background.js
+++ background.js
@@ -9,13 +9,34 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
     console.log(details.url);
-    if( details.url == "https://cdn.sharelatex.com/minjs/ide/pdf/controllers/PdfController.js" )
-      console.log("replacing");
-      return {redirectUrl: "https://raw.githubusercontent.com/agajews/hltex-chrome/master/PdfController.js" };
+      // if(details.url == "https://cdn.sharelatex.com/minjs/ide/pdf/controllers/PdfController.js") {
+      //     console.log("replacing");
+      //     return {redirectUrl: "https://raw.githubusercontent.com/agajews/hltex-chrome/master/PdfController.js"};
+      // }
+      if(details.url == "https://cdn.sharelatex.com/minjs/libs/raven-3.27.0.min.js") {
+        return {redirectUrl: "https://raw.githubusercontent.com/agajews/hltex-chrome/master/raven-3.27.0.min.js"};
+      }
   },
   // {urls: ["*://cdn.sharelatex.com/*.js"]},
-  {urls: ["<all_urls>"]},
+  {urls: ["*://cdn.sharelatex.com/*.js"]},
   ["blocking"]);
+
+chrome.webRequest.onHeadersReceived.addListener(details => {
+    let myResponseHeaders = details.responseHeaders;
+    let header = myResponseHeaders.find(e => e.name == 'Content-Type');
+
+    // Check if the header has been defined already
+    if (header) {
+        console.log ('Modifying header');
+        let headerIndex = myResponseHeaders.indexOf(header);
+        myResponseHeaders.splice(headerIndex,1);
+    }
+
+    myResponseHeaders.push({ name: 'Content-Type', value: 'application/x-javascript' });
+
+    return {responseHeaders: myResponseHeaders};
+}, {urls: ["https://raw.githubusercontent.com/agajews/hltex-chrome/master/raven-3.27.0.min.js"]},
+    ['blocking', 'responseHeaders']);
 
 // chrome.webRequest.onBeforeRequest.addListener(
 //   function(details) { return {cancel: true}; },
