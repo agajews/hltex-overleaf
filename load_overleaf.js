91a4b27ac05bc185521303c9f04e0199d5a4c39c
Alex Gajewski <agajews@gmail.com>
2019-01-07 19:03:09 -0800

    it works again!

--- load_overleaf.js
+++ load_overleaf.js
@@ -1,3 +1,31 @@
+// chrome.runtime.sendNativeMessage('com.hltex.overleaf',
+//     { text: "Hello" },
+//     function(response) {
+//         console.log("Received " + response);
+//     });
+
+// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+//     console.log(response);
+// });
+
+// var toparse = document.createElement("meta");
+// toparse.id = "toparse";
+// toparse.style = { visibility: 'hidden', height: '0px' };
+// document.head.appendChild(toparse);
+
+document.addEventListener('readytoparse', function(e) {
+    e.preventDefault();
+    console.log('Received readytoparse');
+    console.log(e.detail);
+    // tocompile = toparse.textContent;
+    // console.log(tocompile);
+    // toparse.textContent = '';
+
+    // toparse.textContent = tocompile;
+    e = new CustomEvent('readytocompile', { detail: e.detail });
+    document.dispatchEvent(e);
+})
+
 function injectScript(file, node) {
     var th = document.getElementsByTagName(node)[0];
     var s = document.createElement('script');
@@ -5,4 +33,5 @@ function injectScript(file, node) {
     s.setAttribute('src', file);
     th.appendChild(s);
 }
+
 injectScript(chrome.extension.getURL('overleaf.js'), 'body');
--- overleaf.js
+++ overleaf.js
@@ -1,4 +1,5 @@
 console.log('hello from overleaf!');
+
 interval = setInterval(function() {
     if (window._ide) {
         var texid = "5c32d60c21c0266007c09bbd"
@@ -7,23 +8,41 @@ interval = setInterval(function() {
 
         console.log('Setting compiler');
         oldRecompile = window._ide.$scope.recompile;
+        window.oldRecompile = oldRecompile;
+        // toparse = document.getElementById('toparse');
         window._ide.$scope.recompile = function() {
             console.log('recompiling!');
+
+            // toparse.textContent = window._ide.editorManager.getCurrentDocValue();
+            e = new CustomEvent('readytoparse', { detail: window._ide.editorManager.getCurrentDocValue() });
+            document.dispatchEvent(e);
+        }
+
+        document.addEventListener('readytocompile', function(e) {
+            e.preventDefault();
+            console.log('Received readytocompile');
+            console.log(e.detail);
+            // compiled = toparse.textContent;
+            // console.log(compiled);
+            // toparse.textContent = '';
+
             idecopy.socket.emit('joinDoc', texid, { encodeRanges: true }, function (error, docLines, version, updates, ranges) {
                 idecopy.socket.emit('applyOtUpdate', texid, { doc: texid, op: [
                     {"p": 0, "d": docLines.join('\n')},
-                    {"p": 0, "i": window._ide.editorManager.getCurrentDocValue()}
+                    {"p": 0, "i": e.detail}
                 ], v: version }, function(error) {
                     console.log(error);
                     oldRecompile({});
                 })
             });
-        }
+
+        });
 
         var old_element = document.getElementsByClassName('btn-recompile')[0];
         var new_element = old_element.cloneNode(true);
         old_element.parentNode.replaceChild(new_element, old_element);
-        new_element.addEventListener('click', function() {
+        new_element.addEventListener('click', function(e) {
+            e.preventDefault();
             window._ide.$scope.recompile()
         });
 
