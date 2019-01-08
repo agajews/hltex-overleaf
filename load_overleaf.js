bc5129fac7cecec0ed01061f1e226caebae894b5
Alex Gajewski <agajews@gmail.com>
2019-01-07 18:32:50 -0800

    the version that works but doesnt

--- load_overleaf.js
+++ load_overleaf.js
@@ -1,30 +1,3 @@
-// chrome.runtime.sendNativeMessage('com.hltex.overleaf',
-//     { text: "Hello" },
-//     function(response) {
-//         console.log("Received " + response);
-//     });
-
-// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-//     console.log(response);
-// });
-
-var toparse = document.createElement("p");
-toparse.id = "toparse";
-toparse.style = { visibility: 'hidden', height: '0px' };
-document.body.appendChild(toparse);
-
-toparse.addEventListener('readytoparse', function(e) {
-    e.preventDefault();
-    console.log('Received readytoparse');
-    tocompile = toparse.textContent;
-    console.log(tocompile);
-    toparse.textContent = '';
-
-    toparse.textContent = tocompile;
-    event = new Event('readytocompile');
-    toparse.dispatchEvent(event);
-})
-
 function injectScript(file, node) {
     var th = document.getElementsByTagName(node)[0];
     var s = document.createElement('script');
@@ -32,5 +5,4 @@ function injectScript(file, node) {
     s.setAttribute('src', file);
     th.appendChild(s);
 }
-
 injectScript(chrome.extension.getURL('overleaf.js'), 'body');
--- overleaf.js
+++ overleaf.js
@@ -1,5 +1,4 @@
 console.log('hello from overleaf!');
-
 interval = setInterval(function() {
     if (window._ide) {
         var texid = "5c32d60c21c0266007c09bbd"
@@ -8,40 +7,23 @@ interval = setInterval(function() {
 
         console.log('Setting compiler');
         oldRecompile = window._ide.$scope.recompile;
-        window.oldRecompile = oldRecompile;
-        toparse = document.getElementById('toparse');
         window._ide.$scope.recompile = function() {
             console.log('recompiling!');
-
-            event = new Event('readytoparse');
-            toparse.textContent = window._ide.editorManager.getCurrentDocValue();
-            toparse.dispatchEvent(event);
-        }
-
-        toparse.addEventListener('readytocompile', function(e) {
-            e.preventDefault();
-            console.log('Received readytocompile');
-            compiled = toparse.textContent;
-            console.log(compiled);
-            toparse.textContent = '';
-
             idecopy.socket.emit('joinDoc', texid, { encodeRanges: true }, function (error, docLines, version, updates, ranges) {
                 idecopy.socket.emit('applyOtUpdate', texid, { doc: texid, op: [
                     {"p": 0, "d": docLines.join('\n')},
-                    {"p": 0, "i": compiled}
+                    {"p": 0, "i": window._ide.editorManager.getCurrentDocValue()}
                 ], v: version }, function(error) {
                     console.log(error);
                     oldRecompile({});
                 })
             });
-
-        })
+        }
 
         var old_element = document.getElementsByClassName('btn-recompile')[0];
         var new_element = old_element.cloneNode(true);
         old_element.parentNode.replaceChild(new_element, old_element);
-        new_element.addEventListener('click', function(e) {
-            e.preventDefault();
+        new_element.addEventListener('click', function() {
             window._ide.$scope.recompile()
         });
 
