6ec05a1a2133b7a7226432958bfafa76c565849a
Alex Gajewski <agajews@gmail.com>
2019-01-09 21:16:04 -0800

    cleaned up background.js

--- background.js
+++ background.js
@@ -1,49 +1,5 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            console.log(this.response);
-        }
-    }
-    xhr.open('GET', "file:///tmp/test.json");
-    xhr.responseType = 'text';
-    xhr.send();
-});
-
-// chrome.webRequest.onBeforeRequest.addListener(
-//     function(details) {
-//         console.log(details.url);
-//         // if(details.url == "https://cdn.sharelatex.com/minjs/ide/pdf/controllers/PdfController.js") {
-//         //     console.log("replacing");
-//         //     return {redirectUrl: "https://raw.githubusercontent.com/agajews/hltex-chrome/master/PdfController.js"};
-//         // }
-//         if(details.url == "https://cdn.sharelatex.com/minjs/libs/raven-3.27.0.min.js") {
-//             return {redirectUrl: "https://raw.githubusercontent.com/agajews/hltex-chrome/master/raven-3.27.0.min.js"};
-//         }
-//     },
-//     // {urls: ["*://cdn.sharelatex.com/*.js"]},
-//     {urls: ["*://cdn.sharelatex.com/*.js"]},
-//     ["blocking"]);
-
-chrome.webRequest.onHeadersReceived.addListener(details => {
-    let myResponseHeaders = details.responseHeaders;
-    let header = myResponseHeaders.find(e => e.name == 'Content-Type');
-
-    // Check if the header has been defined already
-    if (header) {
-        console.log ('Modifying header');
-        let headerIndex = myResponseHeaders.indexOf(header);
-        myResponseHeaders.splice(headerIndex,1);
-    }
-
-    myResponseHeaders.push({ name: 'Content-Type', value: 'application/x-javascript' });
-
-    return {responseHeaders: myResponseHeaders};
-}, {urls: ["https://raw.githubusercontent.com/agajews/hltex-chrome/master/raven-3.27.0.min.js"]},
-    ['blocking', 'responseHeaders']);
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         async function translate() {
